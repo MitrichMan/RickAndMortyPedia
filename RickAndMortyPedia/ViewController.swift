e74983c341107fad60b727150ab25498289e86a9
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-03-23 16:51:09 +0800

    Initial Commit

---
+++ RickAndMortyPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyPedia
+//
+//  Created by Dmitrii Melnikov on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
